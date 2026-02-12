0bc7f715a25c445af668a8d421c65869af04ccea
vyshak15sp <60498158+vyshak15sp@users.noreply.github.com>
2026-02-12 11:49:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("a[href^='#']").forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
